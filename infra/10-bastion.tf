e4fae1fd509eb0b50921bfbc45fda4a392424b8f
pmh-only <opensource@pmh.codes>
2023-05-17 19:47:41 +0900

    add manifesto

--- infra/10-bastion.tf
+++ infra/10-bastion.tf
@@ -78,6 +78,10 @@ resource "aws_instance" "bastion" {
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+
     yum install -y docker jq curl
 
     usermod -aG docker ec2-user
